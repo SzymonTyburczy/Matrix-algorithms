cde2baac70ed4127b01a212d54b231b274b3ec31
Szymon Tyburczy <SzymonTyburczy@protonmail.com>
2025-11-06 08:32:18 +0100

    nice

--- lab1/src/matrix_Strassen.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "helperFunctions.h"
-Matrix multiply_strassen_wrapper(const Matrix &A, const Matrix &B, unsigned long long &op_count);
--- lab1/src/matrix_ai.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "helperFunctions.h"
-Matrix multiply_ai_recursive_wrapper(const Matrix &A, const Matrix &B, unsigned long long &op_count);
