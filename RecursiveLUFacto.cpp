8f95cf377996362c8feab00828bbc2086edd6bc2
Szymon Tyburczy <SzymonTyburczy@protonmail.com>
2025-11-06 21:15:50 +0100

    n

---
+++ RecursiveLUFacto.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <vector>
+using Matrix = std::vector<std::vector<int>>;
+
+
+int main(){
+    std::vector<int> matrix_sizes = {}; 
+    for(int size = 2; size <= 1000; size++){
+        matrix_sizes.emplace_back(size);
+    }
+    
+
+
+
+    return 0;
+}
---
+++ RecursivelyInversingMatrix.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <vector>
+using Matrix = std::vector<std::vector<int>>;
+
+
+//recursively inversing a matrix
+int main(){
+    std::vector<int> matrix_sizes = {}; 
+    for(int size = 2; size <= 1000; size++){
+        matrix_sizes.emplace_back(size);
+    }
+
+
+
+
+    return 0;
+}
